422d0b344a148f5becaa1931e9919503c877c7fe
Juan Carlos Pazmino <juankpro@outlook.com>
2016-05-09 23:20:37 -0500

    Chapter 2

---
+++ Chapter2/Exercises.hs
@@ -0,0 +1,25 @@
+module Exercises where
+
+-- Parenthesization
+-- 1. 2 + (2 * 3) - 1
+-- 2. (^) 10 (1 + 1)
+-- 3. ((2 ^ 2) * (4 ^ 5)) + 1
+
+-- 1. 2.
+
+-- 1. 1135, 1135, -1110, 1110
+-- 2.
+-- 3. 3375
+-- 4. 5. 6.
+
+waxOn = x * 5
+  where
+    x = y ^ 2
+    y = z + 8
+    z = 7
+
+triple x = x * 3
+
+waxOff x = triple x
+
+-- 6.
---
+++ Chapter2/Intermission.hs
@@ -0,0 +1,25 @@
+module Intermission where
+
+-- 1.
+-- let half x = x / 2
+-- let square x = x * x
+
+-- 2.
+circleArea r = 3.14 * (r * r)
+
+-- 1. changes result
+-- 2. does not changes result
+-- 3. changes result
+
+-- 1. let area x = 3.14 * (x * x) -- removed space
+-- 2. let double x = x * 2 -- replaced b with x
+-- 3. removed initial space in second line
+x = 7
+y = 10
+f = x + y
+
+-- 1. 5
+-- 2. 25
+-- 3. 30
+-- 4. 6
+--
